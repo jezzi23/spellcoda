7c2a2295917f2faab38de0b2db9531b5b906a3d2
jezzi23 <jezzi23@gmail.com>
2025-07-23 18:25:33 +0000

    LLM localization translations added
    Turned off be default. May serve as a starting point if anyone wants to
    implement/improve a localization.

--- locale/ptPT.lua
+++ locale/ptPT.lua
@@ -1,1 +1,6 @@
-local _, sc                    = ...;
+local _, sc = ...; if sc.locale ~= "ptPT" then return; end
+local L = {}; sc.L = L;
+---------------------------------------------------------------------------------------------------
+-- You can get the missing or obsolete strings from the running addon at /spellcoda config
+-- Translated strings should be below this
+---------------------------------------------------------------------------------------------------
